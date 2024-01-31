3283db24ec359c34be12ab3500d818ebb96d80cc
Luiz Henrique Carneiro Carvalho <luizhenriqueccarvalho@gmail.com>
2024-01-31 10:57:35 -0300

    Update settings.py

--- unmep/settings.py
+++ unmep/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-c_$@pjs64(34z^tkxt+2_y^*g1xdz!6czs9bm-w8-^@oyn5w%v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://unmep-heroku-3ad334867682.herokuapp.com/']
+ALLOWED_HOSTS = ['unmep-heroku-3ad334867682.herokuapp.com']
 
 
 # Application definition
